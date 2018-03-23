5442463ef84a4fadf10f73f11e3d51135153a678
Macbook <macbook@macbook-air-macbook.home>
2018-03-23 16:54:15 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Macbook on 22.03.2018.
+//  Copyright © 2018 Rafał Wawrzonkiewicz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done: Bool = false
+    
+}
